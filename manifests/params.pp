df681df51ef5440ef8b1d095a8925e46e4b58cb4
Adam Breaux <breauxaj@gmail.com>
2017-04-27 18:25:34 -0400

    new scaffolding

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,28 +9,39 @@
 class php56::params {
   $php_package_ensure = 'latest'
 
-  case $::operatingsystem {
-    'Amazon': {
-      $php_packages = [
-        'php56',
-        'php56-bcmath',
-        'php56-cli',
-        'php56-common',
-        'php56-dba',
-        'php56-devel',
-        'php56-gd',
-        'php56-intl',
-        'php56-ldap',
-        'php56-mbstring',
-        'php56-mcrypt',
-        'php56-mysqlnd',
-        'php56-pdo',
-        'php56-process',
-        'php56-xml'
-      ]
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystem {
+        Amazon: {
+          case $::operatingsystemmajrelease {
+            default: {
+              $php_packages = [
+                'php56',
+                'php56-bcmath',
+                'php56-cli',
+                'php56-common',
+                'php56-dba',
+                'php56-devel',
+                'php56-gd',
+                'php56-intl',
+                'php56-ldap',
+                'php56-mbstring',
+                'php56-mcrypt',
+                'php56-mysqlnd',
+                'php56-pdo',
+                'php56-process',
+                'php56-xml'
+              ]
+            }
+          }
+        }
+        default: {
+          fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+        }
+      }
     }
     default: {
-      fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
 
